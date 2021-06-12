112a7f88f1916ee8a303b3230ce23bbbdbbf5463
zhaoyunfei <13581668530@163.com>
2021-06-12 17:03:18 +0800

    init

---
+++ test.py
@@ -0,0 +1,11 @@
+# coding=UTF-8
+# print('python共有四种数据类型：数字（int、long、float、complex）、字符串、列表、元组、字典')
+str = 'python共有四种数据类型：数字（int、long、float、complex）、字符串、列表、元组、字典'
+int = -10
+int2 = 3
+long = 0x1234
+float = 0.10
+list = [1,'str','str1',2]
+tuple = (1,'str-tuple')
+dictionary = {'dic1':'value1','dic2':'val2'}
+print int/int2
